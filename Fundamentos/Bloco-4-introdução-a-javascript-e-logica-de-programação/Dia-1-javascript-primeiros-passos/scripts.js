02899f7b97c136e2fd1b73f7a985df85764129e2
oGabrielDev <gabriel.thiago611@gmail.com>
2022-03-07 19:20:41 -0300

    Resolução do Exercicios 1 e 2 .

---
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-logica-de-programação/Dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,22 @@
+// Exercicio Nº 1.
+
+// let a = 25;
+// let b = 10; 
+// console.log (a + b );
+// console.log (a - b);
+// console.log (a * b);
+// console.log (a / b);
+// console.log (a % b);
+
+// Exercicio nº 2 
+
+// const c = 50;
+// const d = 70;
+// if ( c > d) {
+//   console.log ('false')
+// } else {
+//   console.log ('true')
+// }
+
+// Exercicio Nº 3
+
