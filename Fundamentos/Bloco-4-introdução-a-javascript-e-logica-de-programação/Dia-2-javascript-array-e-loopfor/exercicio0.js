51c0d3e57e4c5d40bc3119b7a4f4490c9298693b
oGabrielDev <gabriel.thiago611@gmail.com>
2022-03-08 20:19:54 -0300

    Resolução do Exercicío 8

--- Fundamentos/Bloco-4-introdução-a-javascript-e-logica-de-programação/Dia-2-javascript-array-e-loopfor/exercicio0.js
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-logica-de-programação/Dia-2-javascript-array-e-loopfor/exercicio0.js
@@ -56,14 +56,24 @@
 //   }
 // }
  
-console.log("Exercicío Nº 7");
+// console.log("Exercicío Nº 7");
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menor = numbers[0];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let menor = numbers[0];
 
-for (let i = 1; i < numbers.length; i += 1) {
-  if (menor > numbers[i]){
-    menor = numbers[i]
-  }
-}
-console.log("O menor número do vetor é: ", menor);
+// for (let i = 1; i < numbers.length; i += 1) {
+//   if (menor > numbers[i]){
+//     menor = numbers[i]
+//   }
+// }
+// console.log("O menor número do vetor é: ", menor);
+
+   console.log ("Exercicío Nº 8");
+// let numbers = [];
+
+// for (let index = 1; index <= 25; index+= 1) {
+//   numbers.push(index)
+  
+// }
+
+// console.log(numbers);
