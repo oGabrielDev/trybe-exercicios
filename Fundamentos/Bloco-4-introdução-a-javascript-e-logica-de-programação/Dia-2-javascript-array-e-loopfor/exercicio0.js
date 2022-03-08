bb9ed3b5de5d6520228fccd50d055c724370fe1b
oGabrielDev <gabriel.thiago611@gmail.com>
2022-03-08 15:00:49 -0300

    Resolução dos exercicios 1 e 2

---
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-logica-de-programação/Dia-2-javascript-array-e-loopfor/exercicio0.js
@@ -0,0 +1,13 @@
+// console.log("Exercicío Nº 1");
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// console.log(numbers);
+
+// console.log("Exercicío Nº 2");
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let sum of numbers) {
+//   sum +=1
+//   console.log(sum);
+// }
+
+console.log("Exercicío Nº 3 ");
