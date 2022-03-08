b02ab11e124b7c536d6fd93f739b968b625ed48a
oGabrielDev <gabriel.thiago611@gmail.com>
2022-03-08 18:51:59 -0300

    Resolução do Exercicío 5

--- Fundamentos/Bloco-4-introdução-a-javascript-e-logica-de-programação/Dia-2-javascript-array-e-loopfor/exercicio0.js
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-logica-de-programação/Dia-2-javascript-array-e-loopfor/exercicio0.js
@@ -33,4 +33,15 @@
 //   console.log("Valor menor que 20");
 // }
 
+// console.log("Exercicío Nº 5");
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let maior = numbers [0];
+
+// for (let i = 1; i < numbers.length; i += 1) {
+//   if (maior < numbers[i]){
+//     maior = numbers[i]
+//   }
+// }
+// console.log("O maior numero do vetor é: ", maior);
+
 
