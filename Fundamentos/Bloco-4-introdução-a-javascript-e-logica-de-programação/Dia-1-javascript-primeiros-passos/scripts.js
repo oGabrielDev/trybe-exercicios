b260e7e21cf927aa9b8fa40cc36ab25fed95b80b
oGabrielDev <gabriel.thiago611@gmail.com>
2022-03-08 11:37:30 -0300

    Exercicio 5

--- Fundamentos/Bloco-4-introdução-a-javascript-e-logica-de-programação/Dia-1-javascript-primeiros-passos/scripts.js
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-logica-de-programação/Dia-1-javascript-primeiros-passos/scripts.js
@@ -20,3 +20,35 @@
 
 // Exercicio Nº 3
 
+// const num1 = 125;
+// const num2 = 75;
+// const num3 = 150;
+// if (num1 < num3 < num2) {
+//   console.log ('true')
+// } else {
+//   console.log ('false')
+// }
+
+// Exercicio Nº 4
+
+// const valor = -1;
+// if (valor === 1 ) {
+//   console.log('positivo');
+// } else {
+//   console.log('negativo');
+// }
+
+// Exercicio Nº 5
+
+// const a = 360;
+// const b = 90;
+// const c = 90;
+// if (a - b + c == 180) {
+//   console.log ('false')
+// } else if (a - b - c == 180) {
+//   console.log('true');
+// } else {
+//   console.log('inválido');
+// }
+
+//  Exercicio Nº 6 
