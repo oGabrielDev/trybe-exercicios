094aea54986986a14412b69936edf6cf4ef0043f
oGabrielDev <gabriel.thiago611@gmail.com>
2022-03-08 15:12:22 -0300

    Resolução do exercicío 3

--- Fundamentos/Bloco-4-introdução-a-javascript-e-logica-de-programação/Dia-2-javascript-array-e-loopfor/exercicio0.js
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-logica-de-programação/Dia-2-javascript-array-e-loopfor/exercicio0.js
@@ -12,3 +12,11 @@
 //   console.log(resultado);
 
 // console.log("Exercicío Nº 3 ");
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let resultado = 0
+// for (let i = 0; i < numbers.length; i += 1) {
+//   resultado += numbers[i] / 9
+// }
+//   console.log(resultado);
+
+console.log("Exercicío Nº 4");
