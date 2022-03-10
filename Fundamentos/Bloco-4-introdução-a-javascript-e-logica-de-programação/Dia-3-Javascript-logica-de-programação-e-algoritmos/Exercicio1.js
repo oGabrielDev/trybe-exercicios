b62fc1fad29b35969a912af5a37fca7d22221437
oGabrielDev <gabriel.thiago611@gmail.com>
2022-03-09 23:48:44 -0300

    Exercicío 2, feito com 2 modos para ser executado.

--- Fundamentos/Bloco-4-introdução-a-javascript-e-logica-de-programação/Dia-3-Javascript-logica-de-programação-e-algoritmos/Exercicio1.js
+++ Fundamentos/Bloco-4-introdução-a-javascript-e-logica-de-programação/Dia-3-Javascript-logica-de-programação-e-algoritmos/Exercicio1.js
@@ -2,24 +2,41 @@
 // O fatorial é representado pelo sinal !
 // 4! = 4 x 3 x 2 x 1 = 24
 // Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
-let factory = 1;
+/*let factory = 1;
 
 for (let i= 10; i > 0; i -= 1) {
   factory *= i;
 }
 console.log (factory);
-
+*/
 
 // 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 // let word = 'tryber';
 
+// let word = 'tryber';
+// let newWord = '';
+// for (let i = word.length -1; i >= 0; i -= 1) {
+//   newWord += word[i];
+// }
+// console.log(newWord);
 
+
+
+// let word = 'trybe';
+// let reverseWord = '';
+
+// reverseWord = word.split('').reverse().join('');
+
+// console.log(reverseWord);
 
 
 // 3- Considere o array de strings abaixo:
 // let array = ['java', 'javascript', 'python', 'html', 'css'];
 // Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = Math.max(...array);
+console.log(maior);
 
 
 
